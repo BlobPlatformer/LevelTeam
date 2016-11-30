b49cba66e5de278af81a31c37c7002942d603b2f
Cody Wilhelm <Cody Wilhelm>
2016-11-30 14:46:16 -0600

    updated files

--- TestLevel/src/app.js
+++ TestLevel/src/app.js
@@ -2,22 +2,34 @@
 
 /* Classes and Libraries */
 const Game = require('./game');
+const Player = require('./player');
 const Tiles = require('./tiles');
 
 
 /* Global variables */
 var canvas = document.getElementById('screen');
 var game = new Game(canvas, update, render);
+var player = new Player(0,16*35) ;
 var input = {
   up: false,
   down: false,
   left: false,
   right: false
 }
+var groundHit = false;
 
 
 var spritesheet = new Image();
-spritesheet.src = 'assets/basicTiles.jpg';
+spritesheet.src = 'assets/blobGameLevelSheet.png';
+var spriteArray = [{x: 0, y: 0}, {x: 16, y: 0},{x: 32, y: 0}, {x: 48, y: 0},{x: 64, y: 0}, {x: 80, y: 0},{x: 96, y: 0}, {x: 112, y: 0},{x: 128, y: 0},
+					{x: 0, y: 16}, {x: 16, y: 16},{x: 32, y: 16}, {x: 48, y: 16},{x: 64, y: 16}, {x: 80, y: 16},{x: 96, y: 16}, {x: 112, y: 16},{x: 128, y: 16},
+					{x: 0, y: 32}, {x: 16, y: 32},{x: 32, y: 32}, {x: 48, y: 32},{x: 64, y: 32}, {x: 80, y: 32},{x: 96, y: 32}, {x: 112, y: 32},{x: 128, y: 32},
+					{x: 0, y: 48}, {x: 16, y: 48},{x: 32, y: 48}, {x: 48, y: 48},{x: 64, y: 48}, {x: 80, y: 48},{x: 96, y: 48}, {x: 112, y: 48},{x: 128, y: 48},
+					{x: 0, y: 64}, {x: 16, y: 64},{x: 32, y: 64}, {x: 48, y: 64},{x: 64, y: 64}, {x: 80, y: 64},{x: 96, y: 64}, {x: 112, y: 64},{x: 128, y: 64},
+					{x: 0, y: 80}, {x: 16, y: 80},{x: 32, y: 80}, {x: 48, y: 80},{x: 64, y: 80}, {x: 80, y: 80},{x: 96, y: 80}, {x: 112, y: 80},{x: 128, y: 80},
+					{x: 0, y: 96}, {x: 16, y: 96},{x: 32, y: 96}, {x: 48, y: 96},{x: 64, y: 96}, {x: 80, y: 96},{x: 96, y: 96}, {x: 112, y: 96},{x: 128, y: 96},
+					{x: 0, y: 112}, {x: 16, y: 112},{x: 32, y: 112}, {x: 48, y: 112},{x: 64, y: 112}, {x: 80, y: 112},{x: 96, y: 112}, {x: 112, y: 112},{x: 128, y: 112},
+					{x: 0, y: 128}, {x: 16, y: 128},{x: 32, y: 128}, {x: 48, y: 128},{x: 64, y: 128}, {x: 80, y: 128},{x: 96, y: 128}, {x: 112, y: 128},{x: 128, y: 128}];
 var tiles = new Tiles();
 var map = tiles.getMap();
 var blocks = tiles.getBlocks();
@@ -28,22 +40,27 @@ var blocks = tiles.getBlocks();
  * Handles keydown events
  */
 window.onkeydown = function(event) {
+  //event.preventDefault();
   switch(event.key) {
     case "ArrowUp":
     case "w":
-      
+      input.up = true;
+      event.preventDefault();
       break;
     case "ArrowDown":
     case "s":
-      
+      input.down = true;
+      event.preventDefault();
       break;
     case "ArrowLeft":
     case "a":
-      
+      input.left = true;
+      event.preventDefault();
       break;
     case "ArrowRight":
     case "d":
-      
+      input.right = true;
+      event.preventDefault();
       break;
   }
 }
@@ -53,23 +70,35 @@ window.onkeydown = function(event) {
  * Handles keydown events
  */
 window.onkeyup = function(event) {
+  //event.preventDefault();
   switch(event.key) {
     case "ArrowUp":
     case "w":
-      
+      input.up = false;
+      event.preventDefault();
       break;
     case "ArrowDown":
     case "s":
-      
+      input.down = false;
+      event.preventDefault();
       break;
     case "ArrowLeft":
     case "a":
-      
+      input.left = false;
+      event.preventDefault();
       break;
     case "ArrowRight":
     case "d":
-      
+      input.right = false;
+      event.preventDefault();
       break;
+  }
+}
+
+window.onkeypress = function(event) {
+  event.preventDefault();
+  if(event.keyCode == 32 || event.keyCode == 31) {
+    player.jump();
   }
 }
 
@@ -93,7 +122,17 @@ masterLoop(performance.now());
  * the number of milliseconds passed since the last frame.
  */
 function update(elapsedTime) {
-	
+	player.update(elapsedTime, input);
+  if(player.velocity.y >= 0) {
+    if(tiles.isFloor(player.position)) {
+      //player.velocity = {x:0,y:0};
+      player.velocity.y = 0;
+      player.floor = (Math.floor((player.position.y+32)/16) * 16) - 32;
+    }
+    else {
+      player.floor = canvas.height - 32;
+    }
+  }
 }
 
 
@@ -105,16 +144,25 @@ function update(elapsedTime) {
   * @param {CanvasRenderingContext2D} ctx the context to render to
   */
 function render(elapsedTime, ctx) {
-  var row;
-  var col;
+  //tilemap level background
+  var row = 0;
+  var col = 0;
   for(var i=0; i<map.length; i++) {
-    row = i%tiles.getWidth();
-    col = Math.floor(i/tiles.getWidth());
+    //row = i%tiles.getWidth();
+    //col = Math.floor(i/tiles.getWidth());
     ctx.drawImage(
 		spritesheet,
-        (map[i]-1)*16,0,16,16,
-        row*16,col*16,16,16
-	);
+        spriteArray[map[i]-1].x,spriteArray[map[i]-1].y,16,16,
+        col*16,row*16,16,16
+	  );
+	  col++;
+	  if(col == tiles.getWidth()){
+		  row ++;
+		  col = 0;
+	  }
   }
+
+  //player
+  player.render(elapsedTime, ctx);
 }
 
