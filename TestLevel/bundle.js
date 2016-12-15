d26df3fb2ac787528797407104246346f6964674
Matthew Segraves <matthew.james.segraves@gmail.com>
2016-12-15 17:12:02 -0600

    added new player

--- TestLevel/bundle.js
+++ TestLevel/bundle.js
@@ -348,6 +348,12 @@ Game.prototype.loop = function(newTime) {
 /* Constants */
 const CANVAS_WIDTH = 1120;
 const CANVAS_HEIGHT = 800;
+const MS_PER_FRAME = 1000/8;
+const FRAME = {source_frame_width: 64,
+               source_frame_height: 64,
+               dest_frame_width: 32,
+               dest_frame_height: 32
+};
 /**
  * @module Player
  * A class representing a player's helicopter
@@ -360,7 +366,7 @@ module.exports = exports = Player;
  * @param {BulletPool} bullets the bullet pool
  */
 function Player(x,y) {
-  this.state = "idle-right";
+  this.state = "idle";
   this.position = {x: x, y: y};
   this.redicule = {x: x, y: y};
   this.velocity = {x: 0, y: 0};
@@ -368,7 +374,17 @@ function Player(x,y) {
   this.floor = 640;
   // TODO
   this.img = new Image()
-  this.img.src = 'assets/img/Individual_Img/idle_right.png';
+  //this.img.src = 'assets/img/Individual_Img/idle_right.png';
+  this.img.src = 'assets/img/Sprite_Sheets/animations.png';
+
+  this.actualFrame = {x: 1, y: 0}; // Position of an inner frame in sprite sheet`
+  this.frame = FRAME; // Frame properties, width, height of source and destination
+  this.direction = "right";
+  this.time = MS_PER_FRAME;
+
+  this.storedFH = 0;
+  this.storedF = 0;
+  this.previousState = "moving";
 }
 
 /**
@@ -379,30 +395,161 @@ function Player(x,y) {
  * boolean properties: up, left, right, down
  */
 Player.prototype.update = function(elapsedTime, input) {
-  if (this.position.y >= this.floor) { this.state = "idle-right";}
   switch (this.state) {
-    case "idle-right":
+    case "idle":
+      this.time += elapsedTime;
+      // landing
+      if (this.previousState == "falling") {
+        if (this.time <= MS_PER_FRAME) {
+          this.actualFrame.y = 3;
+          this.actualFrame.x = 3;
+        }
+        else if (this.time <= 2*MS_PER_FRAME) {
+          this.actualFrame.y = 3;
+          this.actualFrame.x = 2;
+        }
+        else if (this.time <= 3*MS_PER_FRAME) {
+          this.actualFrame.y = 3;
+          this.actualFrame.x = 1;
+        }
+        else if (this.time <= 4*MS_PER_FRAME) {
+          this.actualFrame.y = 3;
+          this.actualFrame.x = 0;
+        }
+        else {
+          this.actualFrame.y = this.storedFH;
+          this.actualFrame.x = this.storedF;
+        }
+      }
+      else if (this.previousState == "moving") {
+        if (this.time <= MS_PER_FRAME) {
+          this.actualFrame.y = this.storedF+1; //bit of a hack here, i can explain in class
+          this.actualFrame.x = 0;
+        }
+        else {
+          this.actualFrame.y = this.storedFH;
+          this.actualFrame.x = this.storedF;
+        }
+      }
+
+
       // set the velocity
       //this.velocity.x = 0;
       if(input.left) {
-        if(this.velocity.x > -6) {
+        this.direction = "left";
+        this.actualFrame.y = 1;
+        this.actualFrame.x = 0;
+        this.time = 0;
+        this.state = "moving";
+      }
+      else if(input.right) {
+        this.direction = "right";
+        this.actualFrame.y = 2;
+        this.actualFrame.x = 0;
+        this.time = 0;
+        this.state = "moving";
+      }
+      else {
+        this.velocity.x = 0;
+      }
+      break;
+    case "moving":
+      // set the velocity
+      //this.velocity.x = 0;
+      this.time += elapsedTime;
+      if(input.left) {
+        this.actualFrame.y = 1;
+        if(this.velocity.x > -10) {
+          this.velocity.x -= 1;
+        }
+        if (this.time >= MS_PER_FRAME && this.time <= 2*MS_PER_FRAME) { this.actualFrame.x = 0;}
+        if (this.time >= 2*MS_PER_FRAME) { this.actualFrame.x = 1; }
+      }
+      else if(input.right) {
+        this.actualFrame.y = 2;
+        if(this.velocity.x < 10) {
+          this.velocity.x += 1;
+        }
+        if (this.time >= MS_PER_FRAME && this.time <= 2*MS_PER_FRAME) { this.actualFrame.x = 0;}
+        if (this.time >= 2*MS_PER_FRAME) { this.actualFrame.x = 1; }
+      }
+      else {
+        this.time = 0;
+        this.storedFH = 0;
+        this.previousState = "moving";
+        if (this.direction == "right") { this.storedF = 1;}
+        else this.storedF = 0;
+        this.state = "idle";
+      }
+      break;
+    case "falling":
+      // set the velocity
+      //this.velocity.x = 0;
+      if (this.position.y == this.floor) {
+        this.storedFH = 0;
+        if (this.direction == "left") this.storedF = 0;
+        else this.storedF = 1;
+        this.time = 0;
+        this.state = "idle";
+      }
+      else if(input.left) {
+        if(this.velocity.x > -10) {
           this.velocity.x -= 1;
         }
       }
       else if(input.right) {
-        if(this.velocity.x < 6) {
+        if(this.velocity.x < 10) {
           this.velocity.x += 1;
         }
       }
       else {
         this.velocity.x = 0;
       }
-      //else this.velocity.x = 0;
       break;
     case "jump":
+      this.time += elapsedTime;
+      if (this.time <= MS_PER_FRAME) {
+        this.actualFrame.y = 3;
+        this.actualFrame.x = 0;
+      }
+      else if (this.time <= 2*MS_PER_FRAME) {
+        this.actualFrame.y = 3;
+        this.actualFrame.x = 1;
+      }
+      else if (this.time <= 3*MS_PER_FRAME) {
+        this.actualFrame.y = 3;
+        this.actualFrame.x = 2;
+      }
+      else if (this.time <= 4*MS_PER_FRAME) {
+        this.actualFrame.y = 3;
+        this.actualFrame.x = 3;
+      }
+      else {
+        this.actualFrame.y = 3;
+        this.actualFrame.x = 4;
+      }
 
+      if (this.velocity.y > 0) {
+        this.actualFrame.y = 3;
+        this.actualFrame.x = 4;
+        this.state = "falling";
+      }
+      else if(input.left) {
+        if(this.velocity.x > -10) {
+          this.velocity.x -= 1;
+        }
+      }
+      else if(input.right) {
+        if(this.velocity.x < 10) {
+          this.velocity.x += 1;
+        }
+      }
+      else {
+        this.velocity.x = 0;
+      }
       break;
-  }
+    }
+
 
   // move the player
   this.position.x += this.velocity.x;
@@ -426,11 +573,22 @@ Player.prototype.update = function(elapsedTime, input) {
  * @param {CanvasRenderingContext2D} ctx
  */
 Player.prototype.render = function(elapasedTime, ctx) {
-  ctx.drawImage(this.img, this.redicule.x, this.redicule.y, 32, 32);
+  //ctx.drawImage(this.img, this.redicule.x, this.redicule.y, 32, 32);
+  ctx.drawImage(this.img,
+                this.actualFrame.x * this.frame.source_frame_width,
+                this.actualFrame.y * this.frame.source_frame_height,
+                this.frame.source_frame_width,
+                this.frame.source_frame_height,
+                this.redicule.x,
+                this.redicule.y,
+                this.frame.dest_frame_width,
+                this.frame.dest_frame_height
+  );
 }
 
 Player.prototype.jump = function() {
   if (this.position.y == this.floor) {
+    this.time = 0;
     this.state = "jump";
     this.velocity.y = -30;
   }
